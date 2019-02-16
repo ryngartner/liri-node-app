c097ef12543ed230f63cab731bc4e69bb2b3e4eb
Ryan Gartner <ryanjgartner@gmail.com>
2019-02-15 21:23:45 -0600

    Spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,25 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    for (i = 0; i < data.tracks.items.length; i++){
+        var artistName = data.tracks.items[i].artists.map(function(artist){
+            return artist.name;
+        })
+        console.log(artistName);
+        var artistSong = data.tracks.items[i].name;
+        console.log(artistSong);
+        var artistPre = data.tracks.items[i].preview_url;
+        console.log(artistPre);
+        var artistAlbum = data.tracks.items[i].album.name;
+        console.log(artistAlbum);
+        console.log("---------------------");
+    }
+   
+//   console.log(data.items); 
+
+  });
